b3d2054d86903415ee2315ffbf6a45be54e8ceed
dirkliu <liuqi0702@126.com>
2021-04-17 23:20:23 +0800

    Add exclude and include options

--- index.js
+++ index.js
@@ -4,17 +4,34 @@ function createPxReplace(pixels, viewportSize, unitPrecision, viewportUnit) {
 
 function getQueryString(query){
   if(!query) {
-       return {};
+    return {};
   }
   if(typeof query !== "string" && typeof query !== 'object') {
-  util.error('query need use json type');
-  return {};
-}
-  if(typeof query === "string" && query.substr(0, 1) === "{" && query.substr(-1) === "}"){
-      return JSON.parse(query);
-  }else{
-  return query;
+    console.error('query need use json type');
+    return {};
   }
+  if(typeof query === "string" && query.substr(0, 1) === "{" && query.substr(-1) === "}"){
+    return JSON.parse(query);
+  }else{
+    return query;
+  }
+}
+
+function glob2RegExp (test) {
+	if (typeof test === "string") {
+		test = new RegExp("^" + test.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&"));
+	}
+	return test;
+}
+
+function testRegExpOrGlob (str, test) {
+	if (!test) return true;
+	test = glob2RegExp(test);
+	if (Array.isArray(test)) {
+		return test.map(glob2RegExp).some(regExp => regExp.test(str));
+	} else {
+		return test.test(str);
+	}  
 }
 
 const px2vw = function (source) {
@@ -30,6 +47,14 @@ const px2vw = function (source) {
     decimal:3
   }, getQueryString(this.query))
   var matchPXExp = /([0-9.]+px)([;,| |}|'|"\)\r|\n])/g
+
+  if (options.include && !testRegExpOrGlob(options.include)) {
+    return source
+  }
+
+  if (options.exclude && testRegExpOrGlob(options.exclude)) {
+    return source
+  }
 
   let newSource = source.replace(matchPXExp, function (match, m1, m2) {
     var pixels = parseFloat(m1.slice(0, m1.length - 2));
